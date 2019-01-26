0a3f84ad27e5b0fcb7292398a23f556cb3415f0e
Islam Odeh <islamodeh@Islams-MacBook-Pro.local>
2019-01-26 23:01:54 +0200

    add devise gem and needed configurations + functions + helpers. Create a design for the navbar.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,4 +1,7 @@
 class Instructor < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :courses, dependent: :destroy
-  has_secure_password
 end
