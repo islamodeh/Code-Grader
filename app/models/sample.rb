55321688bc6e34bf5d1877746f92f92ac7ebfc6b
Islam Odeh <islamodeh@Islams-MacBook-Pro.local>
2019-02-24 13:41:33 +0200

    remove /r characters

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -5,5 +5,12 @@ class Sample < ApplicationRecord
 
   # before save, remove extra spaces and lines both from right and left of each sample!
   # for more accurcy
+  before_save :remove_unwanted_characters
+
   scope :sorted, lambda { order(created_at: :asc)}
+  
+  def remove_unwanted_characters
+    self.input = self.input.gsub("\r", "")
+    self.output = self.output.gsub("\r", "")
+  end
 end
