751c53a1381d72bc0bc9727b777c41e7cde8ef6d
Islam Odeh <islamodeh@hotmail.com>
2019-02-27 19:25:27 +0200

    Update mailer problem

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,6 @@ require 'digest/sha2'
 
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
-  default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@gmail.com"
+  default "Message-ID"=>"#{Time.now.to_i.to_s)}"
   layout 'mailer'
 end
